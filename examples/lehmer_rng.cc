04ec2d5e5ee2f474b5a6d4a72b9ed08e7d54c6d2
Sean Brennan <zettix1@gmail.com>
2017-10-12 08:00:42 -0700

    Add large numbers for Lehmer RNG

--- examples/lehmer_rng.cc
+++ examples/lehmer_rng.cc
@@ -9,11 +9,34 @@ namespace com_zettix {
 
 class Lehmer {
  public:
-   Lehmer(const Fatint &seed) : seed_(seed) {
-     n_ = Fatint("1ffffffff");  // 2 << 31 - 1 Merseene prime.
-     g_ = Fatint("41a7"); // 7 ** 5  16,807. primitive root.
-   } 
-
+   Lehmer(const Fatint &seed, int type=1) : seed_(seed) {
+  
+   Fatint f_("5"); 
+   switch (type) {
+     case 1:
+       n_ = Fatint("1ffffffff");  // 2 << 31 - 1 Merseene prime.
+       g_ = Fatint("41a7"); // 7 ** 5  16,807. primitive root.
+       break;
+     case 2:
+       n_ = ONE << 128; 
+       g_ = Fatint("12e15e35b500f16e2e714eb2b37916a5");
+       break;
+     case 3:
+       n_ = Fatint("f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f1");  // Aime' Ferrier.
+       g_ = Fatint("5"); 
+       for (int i = 0; i < 62; i++) {
+         g_ *= f_;
+         int p = i * 100 / 62;
+         std::cout << "Generating g term: " << p << "%" << std::endl;
+       }
+       break;
+     default:
+       n_ = ONE;
+       g_ = ONE;
+       std::cout << "Please pick a Lehmer type." << endl;
+       break;
+     }
+   }
    Fatint next() {
      Fatint result = (g_ * seed_);
      result %= n_;
@@ -29,10 +52,13 @@ class Lehmer {
 
 
 int main(int argc, char **argv) {
-  com_zettix::Fatint seed("12345678901ac1");
-  com_zettix::Lehmer l(seed);
+  int type = 3;
 
-  std::cout << "Lehmer random number generator." << std::endl <<
+  com_zettix::Fatint seed("12345678901ac1");
+  com_zettix::Lehmer l(seed, type);
+
+  std::cout << "Lehmer random number generator type " << type << 
+      "." << std::endl <<
       "based on https://en.wikipedia.org/wiki/Lehmer_random_number_generator"
        << std::endl;
   for (int i = 0; i < 100; i++) {
